ddac1316524b25dbf2b859b3cdba0b02e762f81c
Vitalii Chura <vcvitaly@yahoo.com>
2023-05-26 16:37:25 +0300

    create a draft of hosts command

--- cmd/root.go
+++ cmd/root.go
@@ -60,4 +60,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
 }
